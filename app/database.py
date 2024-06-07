849ccb9b8059376d93e33396faae8e40936d2180
Tushar Chandra <tusharchandra@Tushars-MacBook-Pro.local>
2024-06-07 21:18:02 +0530

    Cleanup

--- app/database.py
+++ app/database.py
@@ -1,6 +1,13 @@
+import time
+import os
+import signal
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/fastapi'
 
@@ -20,3 +27,29 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def shutdown_server():
+    os.kill(os.getpid(), signal.SIGINT)
+    print('Shutting down server')
+
+DB_ATTEMPT_COUNTER = 0
+while DB_ATTEMPT_COUNTER <= 2:
+    try:
+        DB_ATTEMPT_COUNTER = DB_ATTEMPT_COUNTER + 1
+        db_connection = psycopg2.connect(
+            host='localhost',
+            dbname='fastapi',
+            user='postgres',
+            password='root',
+            cursor_factory=RealDictCursor
+        )
+        db_cursor = db_connection.cursor()
+        print('DB Connection successful!!!')
+        break
+    except psycopg2.Error as e:
+        print('DB Connection failed!!!')
+        print(e)
+        if DB_ATTEMPT_COUNTER >= 2:
+            shutdown_server()
+        time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,42 +1,10 @@
-import time
-import os
-import signal
-
 from fastapi import (FastAPI)
-import psycopg2
-from psycopg2.extras import RealDictCursor
 
 import app.models as models
 from app.database import engine
 from app.routers import posts, users, auth
 
 models.Base.metadata.create_all(bind=engine)
-
-
-def shutdown_server():
-    os.kill(os.getpid(), signal.SIGINT)
-    print('Shutting down server')
-
-DB_ATTEMPT_COUNTER = 0
-while DB_ATTEMPT_COUNTER <= 2:
-    try:
-        DB_ATTEMPT_COUNTER = DB_ATTEMPT_COUNTER + 1
-        db_connection = psycopg2.connect(
-            host='localhost',
-            dbname='fastapi',
-            user='postgres',
-            password='root',
-            cursor_factory=RealDictCursor
-        )
-        db_cursor = db_connection.cursor()
-        print('DB Connection successful!!!')
-        break
-    except psycopg2.Error as e:
-        print('DB Connection failed!!!')
-        print(e)
-        if DB_ATTEMPT_COUNTER >= 2:
-            shutdown_server()
-        time.sleep(2)
 
 
 tags_metadata = [
