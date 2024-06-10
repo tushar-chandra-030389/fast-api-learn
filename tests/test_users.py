90835212c399e629d2caeffea5e465eb36612f63
Tushar Chandra <tusharchandra@Tushars-MacBook-Pro.local>
2024-06-10 16:30:07 +0530

    testing login using a fixture to define a user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,20 @@
+import jwt
 from termcolor import colored
+import pytest
 
 import app.schema as schema
+from app.config import config
 from .fixtures import tc, db
+
+@pytest.fixture
+def insert_login_user(tc):
+    user_data = { 'email': 'tushar2@gmail.com', 'password': 'password' }
+    res = tc.post('/users', json=user_data)
+    res_json = res.json()
+
+    assert res.status_code == 201
+    return { **res_json, 'password': user_data['password'] }
+
 
 def test_root(tc):
     res = tc.get('/')
@@ -18,9 +31,15 @@ def test_add_user(tc):
     assert res.status_code == 201
     assert user_res.email == "tushar@gmail.com"
 
-def test_login_user(tc):
-    res = tc.post('/login', data={'username': 'tushar@gmail.com', 'password': 'password'})
+def test_login_user(insert_login_user, tc):
+    res = tc.post('/login', data={'username': insert_login_user['email'], 'password': insert_login_user['password']})
     token_res = schema.TokenResponse(**res.json())
-    
     assert res.status_code == 200
-    assert token_res.access_token is not None
+
+    token_payload = jwt.decode(
+        jwt=token_res.access_token,
+        key=config.secret_key,
+        algorithms=[config.algorithm]
+    )
+    assert int(token_payload['id']) == insert_login_user['id']
+    assert token_res.token_type == 'bearer'
